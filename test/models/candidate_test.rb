a5cc8e92fe9dab15ec3bf94082ce7d80f42a1dbd
Daniel Buompane <danielbuompane@gmail.com>
2016-02-23 14:59:52 -0500

    Test candidates must have name

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -9,4 +9,8 @@ class CandidateTest < ActiveSupport::TestCase
     assert Candidate
   end
 
+  test "candidates must have names" do
+  assert_raises do c = Candidate.create!(hometown: "Raleigh", district: "22nd", party: "Independent")end
+  end
+
 end
