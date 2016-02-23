44531cc946edc19ee8a7729d6828d9ffdb549861
Daniel Buompane <danielbuompane@gmail.com>
2016-02-23 15:09:56 -0500

    Test voter must have unique token

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -8,4 +8,9 @@ class VoterTest < ActiveSupport::TestCase
   test "Voter class exists" do
     assert Voter
   end
+
+  test "voter must have unique token" do
+    a = Voter.create!(name: "Dan", party: "Independent", token: "aaa000")
+    assert_raises do b = Voter.create!(name: "Dave", party: "Republican", token: "aaa000")end
+  end
 end
