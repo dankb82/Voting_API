ffc10615894851b722d7f6ac4b3d38f02c24e349
Daniel Buompane <danielbuompane@gmail.com>
2016-02-24 13:40:17 -0500

    Change verbs in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
-  get 'votes/create'
 
-  get 'votes/destroy'
-
+  post 'votes/create'
+  delete 'votes/destroy'
   get 'votes/index'
 
-  get 'voters/create'
-
+  post 'voters/create'
   get 'voters/show'
-
-  get 'voters/update'
+  patch 'voters/update'
 
   get 'candidates/index'
-
   get 'candidates/show'
-
-  get 'candidates/create'
+  post 'candidates/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
